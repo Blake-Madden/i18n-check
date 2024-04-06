3cf75baf6cc6a7e14eaaaef4ec6e9bc66d0fe2fe
Blake-Madden <madindayton@outlook.com>
2024-04-06 12:42:43 -0400

    Replace old function with string_view

--- src/cpp_i18n_review.cpp
+++ src/cpp_i18n_review.cpp
@@ -699,8 +699,10 @@ namespace i18n_check
                 // example: #define VALUE height, #define VALUE 0x5
                 // No open parentheses after the defined value--then not a function.
                 // Just leave the end marker where it is (EOL) and gobble all of this up.
-                else if (string_util::strnchr(directiveStart, L'(', end - directiveStart) ==
-                         nullptr)
+                else if (directiveStart != nullptr &&
+                         std::wstring_view{ directiveStart,
+                                            static_cast<size_t>(end - directiveStart) }
+                                 .find(L'(') == std::wstring_view::npos)
                     { /*no-op*/
                     }
                 // ...or more like a #defined function, so let main parser deal with it
