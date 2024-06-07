ca9c2114b5b9cfa635fded71493af7693aa1dba0
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-06-07 05:45:53 -0400

    Ignore '%%' in printf strings This are handled in-place; no other printf arguments are used for this

--- src/i18n_review.h
+++ src/i18n_review.h
@@ -989,7 +989,7 @@ namespace i18n_check
         std::wregex m_malformed_html_tag{ LR"(&(nbsp|amp|quot)[^;])" };
         std::wregex m_malformed_html_tag_bad_amp{ LR"(&amp;[[:alpha:]]{3,5};)" };
         std::wregex m_printfCppRE{
-            LR"(([%]([+]|[-] #)?(l)?(d|i|o|u|zu|c|C|e|E|x|X|l|I|I32|I64)|[%]([+]|[-] #)?(l|L)?(f|F)|%s|%p|%%))"
+            LR"(([%]([+]|[-] #)?(l)?(d|i|o|u|zu|c|C|e|E|x|X|l|I|I32|I64)|[%]([+]|[-] #)?(l|L)?(f|F)|%s|%p))"
         };
         std::vector<std::wregex> m_untranslatable_regexes;
 
