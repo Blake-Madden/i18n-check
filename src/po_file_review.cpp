d43709384ed58bda452368669109ea47f6ee6565
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-09-16 19:21:45 -0400

    Ignore commented out entries in PO files

--- src/po_file_review.cpp
+++ src/po_file_review.cpp
@@ -166,6 +166,19 @@ namespace i18n_check
             {
             return std::make_pair(false, std::wstring{});
             }
+        // Step back to see if this is a previous translation (#|) or commented
+        // out translation (#~).
+        size_t lookBehindIndex{ idPos };
+        while (lookBehindIndex > 0 &&
+               string_util::is_neither(poFileText[lookBehindIndex], L'\r', L'\n'))
+            {
+            --lookBehindIndex;
+            }
+        if (poFileText[++lookBehindIndex] == L'#')
+            {
+            return std::make_pair(false, std::wstring{});
+            }
+
         poFileText.remove_prefix(idPos + msgTag.length());
 
         size_t idEndPos{ 0 };
@@ -365,65 +378,64 @@ namespace i18n_check
                 std::wstring originalStr{ catEntry.second.m_source };
                 if (is_untranslatable_string(originalStr, false))
                     {
-                    catEntry.second.m_issues.emplace_back(
-                        translation_issue::unsafe_source_issue,
-                        catEntry.second.m_source);
+                    catEntry.second.m_issues.emplace_back(translation_issue::unsafe_source_issue,
+                                                          catEntry.second.m_source);
                     }
                 }
             if (m_reviewStyles & check_l10n_contains_url)
                 {
                 std::wsmatch results;
- 
+
                 if (std::regex_search(catEntry.second.m_source, results, m_urlEmailRE))
                     {
-                    catEntry.second.m_issues.emplace_back(
-                        translation_issue::unsafe_source_issue,
-                        catEntry.second.m_source);
+                    catEntry.second.m_issues.emplace_back(translation_issue::unsafe_source_issue,
+                                                          catEntry.second.m_source);
                     }
                 }
             if (static_cast<bool>(m_reviewStyles & check_mismatching_printf_commands))
                 {
-            if (catEntry.second.m_po_format == po_format_string::cpp_format)
-                {
-                // only look at strings that have a translation
-                if (!catEntry.second.m_translation.empty())
+                if (catEntry.second.m_po_format == po_format_string::cpp_format)
                     {
-                    printfStrings1 = load_cpp_printf_commands(catEntry.second.m_source, errorInfo);
-                    printfStrings2 =
-                        load_cpp_printf_commands(catEntry.second.m_translation, errorInfo);
-
-                    if (printfStrings1.size() || printfStrings2.size())
+                    // only look at strings that have a translation
+                    if (!catEntry.second.m_translation.empty())
                         {
-                        if (printfStrings1 != printfStrings2)
+                        printfStrings1 =
+                            load_cpp_printf_commands(catEntry.second.m_source, errorInfo);
+                        printfStrings2 =
+                            load_cpp_printf_commands(catEntry.second.m_translation, errorInfo);
+
+                        if (printfStrings1.size() || printfStrings2.size())
                             {
-                            catEntry.second.m_issues.emplace_back(
-                                translation_issue::printf_issue,
-                                L"\"" + catEntry.second.m_source + L"\" vs. \"" +
-                                    catEntry.second.m_translation + L"\"" + errorInfo);
+                            if (printfStrings1 != printfStrings2)
+                                {
+                                catEntry.second.m_issues.emplace_back(
+                                    translation_issue::printf_issue,
+                                    L"\"" + catEntry.second.m_source + L"\" vs. \"" +
+                                        catEntry.second.m_translation + L"\"" + errorInfo);
+                                }
                             }
                         }
-                    }
 
-                if (!catEntry.second.m_translation_plural.empty())
-                    {
-                    printfStrings1 =
-                        load_cpp_printf_commands(catEntry.second.m_source_plural, errorInfo);
-                    printfStrings2 =
-                        load_cpp_printf_commands(catEntry.second.m_translation_plural, errorInfo);
-
-                    if (printfStrings1.size() || printfStrings2.size())
+                    if (!catEntry.second.m_translation_plural.empty())
                         {
-                        if (printfStrings1 != printfStrings2)
+                        printfStrings1 =
+                            load_cpp_printf_commands(catEntry.second.m_source_plural, errorInfo);
+                        printfStrings2 = load_cpp_printf_commands(
+                            catEntry.second.m_translation_plural, errorInfo);
+
+                        if (printfStrings1.size() || printfStrings2.size())
                             {
-                            catEntry.second.m_issues.emplace_back(
-                                translation_issue::printf_issue,
-                                L"\"" + catEntry.second.m_source_plural + L"\" vs. \"" +
-                                    catEntry.second.m_translation_plural + L"\"" + errorInfo);
+                            if (printfStrings1 != printfStrings2)
+                                {
+                                catEntry.second.m_issues.emplace_back(
+                                    translation_issue::printf_issue,
+                                    L"\"" + catEntry.second.m_source_plural + L"\" vs. \"" +
+                                        catEntry.second.m_translation_plural + L"\"" + errorInfo);
+                                }
                             }
                         }
                     }
                 }
             }
-        }
         }
     } // namespace i18n_check
