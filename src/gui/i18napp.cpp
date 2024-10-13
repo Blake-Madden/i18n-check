22fb644f7ff8447118c5ba997444a42c8f566814
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-10-13 10:22:34 -0400

    Add license to repo and About box

--- src/gui/i18napp.cpp
+++ src/gui/i18napp.cpp
@@ -603,8 +603,43 @@ void I18NFrame::OnHelp([[maybe_unused]] wxCommandEvent&)
 //------------------------------------------------------
 void I18NFrame::OnAbout([[maybe_unused]] wxCommandEvent&)
     {
+    wxDateTime buildDate;
+    buildDate.ParseDate(__DATE__);
+
     wxAboutDialogInfo aboutInfo;
     aboutInfo.AddDeveloper(L"Blake Madden");
+    aboutInfo.SetLicence(_DT(LR"(BSD 3-Clause License
+
+Copyright (c) 2021-2024, Blake-Madden
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.)"));
+    aboutInfo.SetCopyright(
+        wxString::Format(_(L"Copyright \U000000A92021-%d %s. All rights reserved."),
+                         buildDate.GetYear(), L"Blake Madden"));
     wxIcon appIcon;
     appIcon.CopyFromBitmap(wxArtProvider::GetBitmap(L"ID_ABOUT", wxART_OTHER, wxSize{ 32, 32 }));
     aboutInfo.SetIcon(appIcon);
