f9338e614b646902856bcc63cad1a10cfd5b7d75
Blake-Madden <66873089+Blake-Madden@users.noreply.github.com>
2024-11-16 10:55:13 -0500

    Update test case

--- tests/cpptests.cpp
+++ tests/cpptests.cpp
@@ -2764,7 +2764,7 @@ TEST_CASE("Context", "[cpp][i18n]")
         cpp.set_style(check_needing_context);
         // weird $ at the end makes this ambiguous
         const wchar_t* code = LR"(SetTitle(wxString::Format(
-        _(L"&Print$"), wxGetApp().GetAppName()));)";
+        _(L"$Print$"), wxGetApp().GetAppName()));)";
         cpp(code, L"");
         cpp.review_strings([](size_t){}, [](size_t, const std::filesystem::path&){ return true; });
         CHECK(cpp.get_localizable_strings_ambiguous_needing_context().size() == 1);
